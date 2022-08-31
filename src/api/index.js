33b245f8469820e16626f7442e90f44cf8dee7f0
Akshay0231 <akshaay2301@gmail.com>
2022-08-31 19:35:49 +0530

    Component| Chart created

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,29 @@
 import axios from "axios";
 
-const url = 'https://covid19.mathdro.id/api'
+const url = "https://covid19.mathdro.id/api";
 
 export const fetchData = async () => {
     try {
-        const { data: { confirmed, deaths, recovered, lastUpdate } } = await axios.get(url)
-        return { confirmed, deaths, recovered, lastUpdate }
+        const {
+            data: { confirmed, deaths, recovered, lastUpdate },
+        } = await axios.get(url);
+        return { confirmed, deaths, recovered, lastUpdate };
     } catch (error) {
-        return error
+        return error;
     }
-}
+};
+
+export const fetchDailyData = async () => {
+    try {
+        const { data } = await axios.get(`${url}/daily`);
+        const modifiedData = data.map((dailyData) => ({
+            confirmed: dailyData.confirmed.total,
+            deaths: dailyData.deaths.total,
+            recovered: dailyData.recovered.total,
+            date: dailyData.date,
+        }));
+        return modifiedData;
+    } catch (error) {
+        return error;
+    }
+};
